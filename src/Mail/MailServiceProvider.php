15a4dfc70ce02cf55f9a1421cce489acac230860
Ben Rutland <ben@10degrees.uk>
2024-10-17 17:11:23 +0100

    updated provider to register shortcodes

--- src/Mail/MailServiceProvider.php
+++ src/Mail/MailServiceProvider.php
@@ -6,7 +6,9 @@ use Navigator\Events\Dispatcher;
 use Navigator\Foundation\Application;
 use Navigator\Foundation\ServiceProvider;
 use Navigator\Mail\Console\Commands\MakeMail;
+use Navigator\View\View;
 use Navigator\View\ViewFactory;
+use Navigator\WordPress\WordPressFactory;
 
 class MailServiceProvider extends ServiceProvider
 {
@@ -23,12 +25,40 @@ class MailServiceProvider extends ServiceProvider
             MakeMail::class,
         ]);
 
+        $view = $this->app->get(ViewFactory::class);
+        $factory = $this->app->get(WordPressFactory::class);
+
         $this->app->get(Dispatcher::class)
-            ->listen('navigator_mailer_content', function (string $content): string {
-                return $this->app->get(ViewFactory::class)->file(
-                    __DIR__ . '/resources/views/template.php',
-                    ['slot' => $content]
+            ->listen('navigator_mailer_content', function (string $content) use ($view): string {
+                return $view->file(
+                    __DIR__ . '/resources/views/layout.php',
+                    ['slot' => do_shortcode($content)]
                 );
             });
+
+        $this->registerShortcodes($factory, $view);
+    }
+
+    public function registerShortcodes(WordPressFactory $factory, ViewFactory $view): void
+    {
+        $factory->registerShortcode('button', function (array $attributes = [], string $slot = '') use ($view): View {
+            return $view->file(__DIR__ . '/resources/views/button.php', [
+                'url'   => $attributes['url'],
+                'color' => $attributes['color'] ?? 'primary',
+                'slot'  => $slot,
+            ]);
+        });
+
+        $factory->registerShortcode('panel', function (array $attributes = [], string $slot = '') use ($view): View {
+            return $view->file(__DIR__ . '/resources/views/panel.php', compact('slot'));
+        });
+
+        $factory->registerShortcode('subcopy', function (array $attributes = [], string $slot = '') use ($view): View {
+            return $view->file(__DIR__ . '/resources/views/subcopy.php', compact('slot'));
+        });
+
+        $factory->registerShortcode('table', function (array $attributes = [], string $slot = '') use ($view): View {
+            return $view->file(__DIR__ . '/resources/views/table.php', compact('slot'));
+        });
     }
 }
