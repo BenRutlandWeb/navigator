2acf087e7bb1ee9f98d348ca003b79bf7b2a3354
Ben Rutland <ben@10degrees.uk>
2024-11-01 15:01:04 +0000

    auto fill http message if none provided

--- src/Http/Exceptions/HttpException.php
+++ src/Http/Exceptions/HttpException.php
@@ -9,6 +9,6 @@ class HttpException extends Exception
 {
     public function __construct(public readonly int $statusCode, string $message = '', public readonly array $headers = [], int $code = 0, ?Throwable $e = null)
     {
-        parent::__construct($message, $code, $e);
+        parent::__construct($message ?: get_status_header_desc($statusCode), $code, $e);
     }
 }
