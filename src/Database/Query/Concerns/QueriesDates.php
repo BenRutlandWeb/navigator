53c1254c20aea7c778d119829084d6a7a621e5ba
Ben Rutland <ben@10degrees.uk>
2024-11-05 16:57:15 +0000

    added more date query helper methods and changed how it interfaces with datequery

--- src/Database/Query/Concerns/QueriesDates.php
+++ src/Database/Query/Concerns/QueriesDates.php
@@ -17,17 +17,33 @@ trait QueriesDates
 
     public function whereDateBefore(string $column, DateTimeInterface|string $date): static
     {
-        return $this->where('date_query', [[
-            'column' => $column,
-            'before' => DateQuery::resolveDateTime($date),
-        ]]);
+        return $this->whereDate(function (DateQuery $query) use ($column, $date) {
+            $query->where($column, null, ['before' => DateQuery::resolveDateTime($date)]);
+        });
     }
 
     public function whereDateAfter(string $column, DateTimeInterface|string $date): static
     {
-        return $this->where('date_query', [[
-            'column' => $column,
-            'after'  => DateQuery::resolveDateTime($date),
-        ]]);
+        return $this->whereDate(function (DateQuery $query) use ($column, $date) {
+            $query->where($column, null, ['after' => DateQuery::resolveDateTime($date)]);
+        });
+    }
+
+    public function whereDateBetween(string $column, DateTimeInterface|string $start, DateTimeInterface|string $end): static
+    {
+        return $this->whereDate(function (DateQuery $query) use ($column, $start, $end) {
+            $query->where($column, null, [
+                'before' => DateQuery::resolveDateTime($end),
+                'after'  => DateQuery::resolveDateTime($start),
+            ]);
+        });
+    }
+
+    public function whereDateNotBetween(string $column, DateTimeInterface|string $start, DateTimeInterface|string $end): static
+    {
+        return $this->whereDate(function (DateQuery $query) use ($column, $start, $end) {
+            $query->where($column, null, ['before' => DateQuery::resolveDateTime($start)])
+                ->orWhere($column, null, ['after'  => DateQuery::resolveDateTime($end)]);
+        });
     }
 }
