169dc8f35082ad090b63fab31a9ba16b76eb78a5
Ben Rutland <ben@10degrees.uk>
2024-11-12 10:39:20 +0000

    updated interface

--- src/Mail/EnvelopeInterface.php
+++ src/Mail/EnvelopeInterface.php
@@ -7,16 +7,16 @@ use Navigator\Contracts\MailableInterface;
 interface EnvelopeInterface
 {
     /** @param iterable<int, MailableInterface|string>|MailableInterface|string $address */
-    public function to(iterable|MailableInterface|string $address, string $name = ''): static;
+    public function to(iterable|MailableInterface|string $address, string $name = ''): EnvelopeInterface;
 
-    public function from(MailableInterface|string $address, string $name = ''): static;
-
-    /** @param iterable<int, MailableInterface|string>|MailableInterface|string $address */
-    public function cc(iterable|MailableInterface|string $address, string $name = ''): static;
+    public function from(MailableInterface|string $address, string $name = ''): EnvelopeInterface;
 
     /** @param iterable<int, MailableInterface|string>|MailableInterface|string $address */
-    public function bcc(iterable|MailableInterface|string $address, string $name = ''): static;
+    public function cc(iterable|MailableInterface|string $address, string $name = ''): EnvelopeInterface;
 
     /** @param iterable<int, MailableInterface|string>|MailableInterface|string $address */
-    public function replyTo(iterable|MailableInterface|string $address, string $name = ''): static;
+    public function bcc(iterable|MailableInterface|string $address, string $name = ''): EnvelopeInterface;
+
+    /** @param iterable<int, MailableInterface|string>|MailableInterface|string $address */
+    public function replyTo(iterable|MailableInterface|string $address, string $name = ''): EnvelopeInterface;
 }
