5b79457a6ba138bdbf31f66fa1bc8c7bf5b8cc72
Ben Rutland <ben@10degrees.uk>
2025-08-15 12:40:29 +0100

    removed session handlers other than db

--- src/Session/SessionServiceProvider.php
+++ src/Session/SessionServiceProvider.php
@@ -3,41 +3,22 @@
 namespace Navigator\Session;
 
 use Navigator\Database\Connection;
-use Navigator\Filesystem\Filesystem;
 use Navigator\Foundation\Application;
 use Navigator\Foundation\ServiceProvider;
 use Navigator\Http\Request;
 use Navigator\Session\Console\Commands\SessionTables;
-use Navigator\Session\Handlers\ArraySessionHandler;
 use Navigator\Session\Handlers\DatabaseSessionHandler;
-use Navigator\Session\Handlers\FileSessionHandler;
-use Navigator\Session\Handlers\NullSessionHandler;
 
 class SessionServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->app->singleton(NullSessionHandler::class, fn() => new NullSessionHandler());
-        $this->app->singleton(ArraySessionHandler::class, fn() => new ArraySessionHandler());
         $this->app->singleton(DatabaseSessionHandler::class, function (Application $app) {
             return new DatabaseSessionHandler($app->get(Connection::class));
         });
-        $this->app->singleton(FileSessionHandler::class, function (Application $app) {
-            return new FileSessionHandler($app->get(Filesystem::class), wp_get_upload_dir()['basedir'] . '/sessions', 120);
-        });
 
         $this->app->singleton(SessionManager::class, function (Application $app) {
-
-            $handlers = [
-                'database' => DatabaseSessionHandler::class,
-                'array'    => ArraySessionHandler::class,
-                'files'    => FileSessionHandler::class,
-                'null'     => NullSessionHandler::class,
-            ];
-
-            $handler = $handlers[$app->env('SESSION_DRIVER')] ?? FileSessionHandler::class;
-
-            return new SessionManager($app->get($handler));
+            return new SessionManager($app->get(DatabaseSessionHandler::class));
         });
 
         $this->app->singleton(Session::class, fn() => new Session());
